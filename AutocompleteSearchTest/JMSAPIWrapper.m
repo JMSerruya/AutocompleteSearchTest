5d2159c7daf4d5101f35f2b2c0af5065dfa26cd8
Juan Manuel Serruya <shuank@gmail.com>
2014-03-31 10:21:49 +0200

    added PodFile for AFNetworking

---
+++ AutocompleteSearchTest/JMSAPIWrapper.m
@@ -0,0 +1,13 @@
+//
+//  JMSAPIWrapper.m
+//  AutocompleteSearchTest
+//
+//  Created by Juan Manuel Serruya on 3/31/14.
+//  Copyright (c) 2014 Juan Manuel Serruya. All rights reserved.
+//
+
+#import "JMSAPIWrapper.h"
+
+@implementation JMSAPIWrapper
+
+@end
